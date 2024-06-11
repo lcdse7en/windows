80352b9fa765f2283f0962e04049b3580b4a4524
lcdse7en <2353442022@qq.com>
2024-06-12 02:12:37 +0800

    add

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ install-scoop() {
     # 方式1
     iwr --useb scoop.201704.xyz | iex
     # 方式2
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
     Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
     # 方式3
 		iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/lukesampson/scoop/master/bin/install.ps1')
@@ -32,6 +33,18 @@ install-scoop() {
     # Step4: 检查Scoop安装情况
     # =====================================
 		scoop help
+
+    # =====================================
+    # 国内镜像加速方案
+    # =====================================
+    scoop bucket add main https://codechina.csdn.net/mirrors/ScoopInstaller/Main.git
+    scoop bucket add extras https://codechina.csdn.net/mirrors/lukesampson/scoop-extras.git
+    scoop bucket add nonportable https://codechina.csdn.net/mirrors/TheRandomLabs/scoop-nonportable
+    scoop bucket add nirsoft https://codechina.csdn.net/mirrors/kodybrown/scoop-nirsoft.git
+    scoop bucket add nerd-fonts https://codechina.csdn.net/mirrors/matthewjberger/scoop-nerd-fonts.git
+    scoop bucket add java https://codechina.csdn.net/mirrors/ScoopInstaller/Java.git
+    scoop bucket add versions https://codechina.csdn.net/mirrors/ScoopInstaller/Versions.git
+    scoop update
 
     # 更换scoop更新镜像
     scoop config SCOOP_REPO "https://gitee.com/glsnames/scoop-installer"
@@ -43,10 +56,14 @@ install-scoop() {
 
     # 拉取新库地址
     scoop update
+
+    # NOTE: 卸载 scoop
+    scoop uninstall scoop
 }
 
 scoop-install-packages() {
-    # https://scoop.sh search apps
+    # search apps
+    # NOTE: http://scoop.sh search apps
     # https://github.com/rasa/scoop-directory | https://rasa.github.io/scoop-directory/search
 
     # 查看代理IP地址
@@ -63,12 +80,30 @@ scoop-install-packages() {
 
     scoop install git # 包含7Zip
     scoop install aria2 # 多线程下载
+    # NOTE: 配置是否使用aria2下载，false为禁用，true为启用
+    scoop config aria2-enabled true
+    scoop install main/neovim
+    scoop install main/typst
+    scoop install main/lua
+    scoop install main/lua-language-server
+    scoop install main/stylua
+    scoop install main/luajit
     scooo install curl
     scoop install ag
-    scoop install go
+    scoop install main/go
     scoop install python
+    scoop install main/ruff
+    scoop install main/pipx
+    scoop install main/rye
+    scoop install extras/spyder
     scoop install ffmpeg
-    scoop install nodejs
+    scoop install main/nodejs
+    scoop install main/rust
+    scoop install main/rustup
+    scoop install main/rust-analyzer
+    scoop install main/just
+    scoop install main/neofetch
+    scoop install main/shfmt
 
     scoop list
 
@@ -86,18 +121,45 @@ scoop-install-packages() {
     # 删除bucket仓库
     scoop bucket rm extras
 
+    scoop install extras/psreadline
+    scoop install extras/wechat
+    scoop install extras/qq
+    scoop install extras/scoop-completion
+    scoop install extras/posh-git
     scoop install snipaste
     scooo install googlechrome
     scooo install firefox
-    scooo install vscode
+    scoop install extras/vscode
     scooo install everything
     scooo install emacs
+    # NOTE: 安装指定版本的软件
+    scooo install freedownloadmanager@6.18.0
     # =====================================
     # Bucket: extras end
     # =====================================
 
-    scoop update
-    scoop update
+    # =====================================
+    # Bucket: versions start
+    # =====================================
+    scoop bucket add versions
+
+    scoop install versions/python312
+    scoop install versions/neovim-nightly
+    scoop install versions/lua53
+    scoop install versions/rust-nightly
+    # =====================================
+    # Bucket: versions end
+    # =====================================
+
+		# =====================================
+		#  Bucket: nerd-fonts start
+		# =====================================
+    scoop bucket add nerd-fonts
+
+    scoop install nerd-fonts/JetBrainsMono-NF
+		# =====================================
+		#  Bucket: nerd-fonts end
+		# =====================================
 
     # 添加第三方bucket仓库
     # scoop bucket add 别名 git地址
